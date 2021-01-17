e76d265f9de2df4cd96d220727b35dcb550533fd
Dax Mickelson <dmickels@cisco.com>
2021-01-16 19:00:37 -0700

    ran black

--- example.py
+++ example.py
@@ -3,16 +3,16 @@ from comment_header_creator import uniline, multiline, _generate_line
 print("uniline() no passed title:")
 print(uniline())
 print("\nuniline() passed title:")
-print(uniline(title='asdfasdf'))
+print(uniline(title="asdfasdf"))
 
 print("\nmultiline() no passed title:")
 print(multiline())
 print("\nmultiline() passed title:")
-print(multiline(title='asdfasdf'))
+print(multiline(title="asdfasdf"))
 
 
 print("\nCall the generator directly.  (Remember it returns a Tuple!)")
-output, status = _generate_line(title='ASDf')
+output, status = _generate_line(title="ASDf")
 print(f"{output}")
 output, status = _generate_line()
 print(f"\n{output}")
