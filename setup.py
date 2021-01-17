a33e3a28474dd7e03a51fa208a9fbb342403ce31
Dax Mickelson <dmickels@cisco.com>
2021-01-16 19:23:02 -0700

    new release

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
 # Changes between releases or new pypi projects
-__VERSON__ = "20200116.1"
+__VERSON__ = "20200116.2"
 __package_name__ = "comment-header-creator"
 __url__ = "https://github.com/daxm/python_comment_header_creator"
-__description__ = (
-    "Create nicely formatted comment strings to be used as headers/seperators in your code.",
-)
+__description__ = "Create nicely formatted comment strings to be used as headers/seperators in your code."
 
 # Stays the same (for me).
 __username__ = "daxm"
 __author__ = "Dax Mickelson"
 __author_email = "dmickels@cisco.com"
 __license__ = "BSD"
-__name__ = f"{__package_name__}-{__username__}"
+__name__ = f"{__package_name__}"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
